76b0ed0bfdf16663173f12a993e24afd031dbfee
Hamzah S <hamzah.saeed@arkhitech.com>
2024-06-05 13:06:52 +0500

    basic express server setup

--- index.js
+++ index.js
@@ -9,7 +9,21 @@
 // console.log(`Celcius to Farenheit ${celciusToFarenheit(0)}`);
 
 
-import getPosts, { getPostsLength } from "./postController.js";
+// import getPosts, { getPostsLength } from "./postController.js";
 
-console.log(getPosts());
-console.log(`Posts length: ${getPostsLength()}`)
+// console.log(getPosts());
+// console.log(`Posts length: ${getPostsLength()}`)
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Hello from Homepage");
+});
+
+app.get("/about", (req, res) => {
+  return res.send(`Hello ${req.query.name}`);
+});
+
+app.listen(8000, () => console.log("Server Started"));
