85a3cc14a128f3b1cecdecff00842df5a1532083
hamzahsaeeds <hamzahsaeed23@hotmail.com>
2024-06-09 14:51:58 +0500

    some basic routes set up and type changed back from es6 to commonjs

--- index.js
+++ index.js
@@ -14,16 +14,51 @@
 // console.log(getPosts());
 // console.log(`Posts length: ${getPostsLength()}`)
 
-import express from "express";
+const express = require("express");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
+const PORT = 8000;
 
-app.get("/", (req, res) => {
-  return res.send("Hello from Homepage");
+// Routes
+
+// server side rendered HTML document response
+app.get('/users', (req, res) => {
+  const html = `
+    <ul>
+      ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+  `;
+  return res.send(html);
 });
 
-app.get("/about", (req, res) => {
-  return res.send(`Hello ${req.query.name}`);
+// endpoint to return JSON data that can be used by clients like mobile devs
+// REST APIs
+app.get('/api/users', (req, res) => {
+  return res.json(users);
 });
 
-app.listen(8000, () => console.log("Server Started")); 
+app
+  .route('/api/users/:id')
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    // TODO: Edit the user with id
+    return res.json({ status: 'pending' });
+  })
+  .delete((req, res) => {
+    // TODO: Delete the user with id
+    return res.json({ status: 'pending' });
+  });
+
+app.post('/api/users', (req, res) => {
+  // TODO: Create new user
+  return res.json({ status: 'pending' });
+});
+
+
+
+app.listen(8000, () => console.log(`Server Started at port ${PORT}`)); 
